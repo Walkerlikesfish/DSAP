8b9fb6a17ffd08573d5d906135c158998b8ae91a
Walker <Walkerlikesfish@users.noreply.github.com>
2017-02-22 11:38:05 +0100

    Update lab1_0.m

--- lab1_0.m
+++ lab1_0.m
@@ -10,10 +10,29 @@ s = y;
 s(:,2) = -s(:,2);
 s = sum(s, 2)/sqrt(2);
 
+%% Upmixing - 2) LMS
+lms2 = dsp.LMSFilter('Length',N_frame, ...
+   'Method','LMS',...
+   'AdaptInputPort',true, ...
+   'StepSizeSource','Input port', ...
+   'WeightsOutputPort',false);
+filt2 = dsp.FIRFilter('Numerator', fir1(10,[.5, .75]));
+x = randn(1000,1); % Noise
+d = filt2(x) + sin(0:.05:49.95)'; % Noise + Signal
+a = 1; % adaptation control
+mu = 0.05; % step size
+[y, err] = lms2(x,d,mu,a);
+
+%% Upmixing - 3) PCA-based
+
+%% Upmixing - 4) ADP
+
 %% LPF 1
 
 %% LPF 2
 
 %% LPF 3
 
-%% 
+%% 90 degree shifter
+
+%% assemble
