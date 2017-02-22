b46c93ed215ccdaf37b717d223244d78daa3c49c
muzineer <nlthuong90@gmail.com>
2017-02-22 11:30:27 +0100

    Passive Surround Decoding Method

--- lab1_0.m
+++ lab1_0.m
@@ -5,6 +5,10 @@ filename = './data/test0.mp3';
 [y,Fs] = audioread(filename);
 
 %% Upmixing - 1) PSD
+c = sum(y, 2)/sqrt(2); %center
+s = y;
+s(:,2) = -s(:,2);
+s = sum(s, 2)/sqrt(2);
 
 %% LPF 1
 
