c0f1e0b4caa72d45c60689d69b789334be835931
Quentin Bolsee <quentinbolsee@hotmail.com>
2017-03-12 21:32:52 +0100

    fixed pca aglorithm, window issue

--- upmix_pca.m
+++ upmix_pca.m
@@ -1,18 +1,5 @@
 function [c, s] = upmix_pca(x_l, x_r)
 
-%% process whole signal at once
-
-cov_mat = cov([x_l x_r]);
-[v, ~] = eig(cov_mat);
-v_s = v(:, 1); % first one: smallest eigenvalue
-v_c = v(:, 2); % second one: greatest eigenvalue
-c = x_l * v_c(1) + x_r * v_c(2);
-s = x_l * v_s(1) + x_r * v_s(2);
-
-return;
-
-%% window-based: incredibly buggy
-
 N = 1024;
 M = 128;
 n = 0:N-1;
@@ -21,6 +8,7 @@ w = zeros(N, 1);
 w(1:M) = sin((pi/2)*(n(1:M)+1/2)/M);
 w(M+1:N-M) = 1;
 w(N-M+1:N) = sin((pi/2)*(N-n(N-M+1:N)-1/2)/M);
+w = w.^2; % square the window, otherwise perfect sum is not achieved
 
 done = false;
 t_begin = 1;
@@ -28,6 +16,16 @@ t_max = length(x_l);
 
 c = zeros(size(x_l));
 s = zeros(size(x_l));
+
+% use running average on the eigenvectors, more natural feel
+lambda = 0.05;
+% initial values
+v_c_inertia = [1; 1]/sqrt(2);
+v_s_inertia = [1; -1]/sqrt(2);
+
+% plot for debug
+v_c_plot_l = [];
+v_c_plot_r = [];
 
 while (~done)
     t_end = t_begin + N - 1;
@@ -46,17 +44,34 @@ while (~done)
     v_c = abs(v_c);
     v_s(1) = abs(v_s(1));
     v_s(2) = -abs(v_s(2));
-
-    c(t_begin:t_end) = c(t_begin:t_end) + (y_l * v_c(1) + y_r * v_c(2));
-    s(t_begin:t_end) = s(t_begin:t_end) + (y_l * v_s(1) + y_r * v_s(2));
+    
+    v_c_inertia = (1-lambda)*v_c_inertia + lambda*v_c;
+    v_s_inertia = (1-lambda)*v_s_inertia + lambda*v_s;
+    
+    c(t_begin:t_end) = c(t_begin:t_end) + (y_l * v_c_inertia(1) + y_r * v_c_inertia(2)).*w(1:t_end-t_begin+1);
+    s(t_begin:t_end) = s(t_begin:t_end) + (y_l * v_s_inertia(1) + y_r * v_s_inertia(2)).*w(1:t_end-t_begin+1);
+    
+    v_c_plot_l = [v_c_plot_l; v_c_inertia(1)];
+    v_c_plot_r = [v_c_plot_r; v_c_inertia(2)];
     
     done = t_end == t_max;
     t_begin = t_end - M + 1;% move window with overlap
 end
 
+% debug mixing
+
+% figure;
+% hold on;
+% plot(d_plot, 'b');
+% plot(d_plot2, 'r');
+
+
 % debug filter
+
 % w_sum = zeros(N+(N-M), 1);
-% w_sum(1:N) = w.^2;
-% w_sum(N-M+1:end) = w_sum(N-M+1:end) + w.^2;
+% w_sum(1:N) = w;
+% w_sum(N-M+1:end) = w_sum(N-M+1:end) + w;
+% figure;
+% plot(w_sum);
 
 end
